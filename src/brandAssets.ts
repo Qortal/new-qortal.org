13bf3ab567c71e27cd800e515e6cd71146cfc820
crowetic <jason@crowetic.com>
2026-04-28 15:32:34 -0700

    NuQloud site update

--- src/brandAssets.ts
+++ src/brandAssets.ts
@@ -1,6 +1,6 @@
-import brandHeaderLogo from "./assets/brand/NuQloud-Cube-logo-logoOnly-NEW-3-512x512.png";
-import brandHeroLogo from "./assets/brand/NuQloud-Cube-logo-OutlineWording-NEW-3-512x512.png";
-import brandFavicon from "./assets/brand/NuQloud-Cube-logo-logoOnly-NEW-3-512x512.png";
+import brandHeaderLogo from "./assets/brand/NuQloud-Cube-logo-logoOnly-NEW-5-512x512.png";
+import brandHeroLogo from "./assets/brand/NuQloud-Cube-logo-OutlineWording-NEW-5-512x512.png";
+import brandFavicon from "./assets/brand/NuQloud-Cube-logo-logoOnly-NEW-5-512x512.png";
 
 export const BRAND_HEADER_LOGO = brandHeaderLogo;
 export const BRAND_HERO_LOGO = brandHeroLogo;
